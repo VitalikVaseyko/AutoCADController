2814c4eb4e40514ea8d0e6d5087dfeab27451d96
Vitalii V. <vvaseyko@ukr.net>
2025-12-14 11:40:32 +0200

    Create AssemblyInfo.cs for AutoCadController
    Added assembly information for the AutoCadController plugin.

---
+++ plugin/AutoCadController/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutoCadController")]
+[assembly: AssemblyDescription("AutoCAD plugin for automated 3D reconstruction from 2D drawings")]
+[assembly: AssemblyCompany("VitalikVaseyko")]
+[assembly: AssemblyProduct("AutoCadController")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
